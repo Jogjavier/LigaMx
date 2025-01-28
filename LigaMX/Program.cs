3e5dece1df99695e73d51742b9c449605d41fbf4
Jogjavier <jog20ortiz@gmail.com>
2025-01-28 17:52:06 -0600

    Edit

--- LigaMX/Program.cs
+++ LigaMX/Program.cs
@@ -3,15 +3,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
 
 app.MapControllerRoute(
@@ -20,10 +11,4 @@ app.MapControllerRoute(
 
 app.Run();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Liga}/{action=Index}/{id?}");
-});
 
